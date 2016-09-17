827b7078059368a5316a901615467549d34c61db
Jordi @ WHITE <jordivilahdl@gmail.com>
2016-09-16 22:25:31 -0600

    stepper galil, siderostat mirrors and momentum wheels

--- Client/src/javasource/MyClient.java
+++ Client/src/javasource/MyClient.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class MyClient {
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-		String serverHostname = new String ("192.168.0.100");
+		String serverHostname = new String ("192.168.0.104");
 
 		if (args.length > 0)
 			serverHostname = args[0];
@@ -37,21 +37,17 @@ public class MyClient {
 		String userInput;
 		String s = null;
 		System.out.println("Write commands..");
-
+		boolean test;
+		char [] cbuf = new char [1];
+		int numRead = 0 ;
 
 		while ((userInput = stdIn.readLine()) != null) {
-			out.println(userInput);
+			
 			if(Objects.equals(userInput, new String ("DL"))){
-				readFileAndDownload("Test.txt",out);
+			
+				readFileAndDownload("CCMG.txt",out);
 			}
-//			do{
-//				s = in.readLine();
-//				// TODO: handle exception
-//				System.out.println(s);
-//			}while(s.length() != 0);
 
-
-			System.out.print ("input: ");
 		}
 
 
@@ -68,8 +64,17 @@ public class MyClient {
 		int c = 0;
 		byte[] buffer = new byte[1];
 		//OutputStream out = null;
+		System.out.println("DL");
+		out.println(new String ("DL"));
+		System.out.println("Aborting previous program.. AB & ST");
+		out.println(new String ("AB"));
+		Thread.sleep(1000);
+		out.println(new String ("ST"));
+		Thread.sleep(1000);
+		System.out.println("DL");
+		out.println(new String ("DL"));
+		Thread.sleep(2000);
 		System.out.println("Opening file"); 
-		Thread.sleep(4000);
 		URL pathFile = MyClient.class.getResource("/textFiles/"+fileName);
 		//Try to open .txt file
 		try {
@@ -90,28 +95,29 @@ public class MyClient {
 
 				System.out.print((char)(buffer[0]));
 				out.print((char)buffer[0]);
+				Thread.sleep(1);
 			}
 			else{	
-				out.println();
+				//out.println();
 				endOfFile = true;
 			}
 			//Now we have removed the 'header'
 		}while(endOfFile == false);
-		Thread.sleep(2000);
+		Thread.sleep(200);
 		out.println(new String("\\"));
 		System.out.println(new String("\\"));
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		out.println(new String("BP"));
 		System.out.println(new String("BP"));
 		System.out.println("Program Burned!");
 		Thread.sleep(3000);
 		System.out.println("Resetting controller..");
 		out.println(new String("RS"));
-		System.out.print(new String("RS"));
-		Thread.sleep(4000);
+		System.out.println(new String("RS"));
+		Thread.sleep(1000);
 		is.close();
 
-		System.out.println("End of file reached !");
+		System.out.println("Done!");
 
 
 
